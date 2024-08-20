af22f148a89f299fe8720bf74d03f995952e4402
cvalverde96 <cvalverde96@gmail.com>
2024-08-20 13:44:22 -0400

    modificando los argumentos en fork _func

--- fork_func.c
+++ fork_func.c
@@ -1,7 +1,7 @@
 #include "shell.h"
 
 /**
- * fork - function que usa fork para fork un nuevo process
+ * fork_func - function que usa fork para fork un nuevo process
  * y ejecuta el commando entrado por usuario
  * @line: pointer que recibe el input
  */
@@ -14,8 +14,12 @@ void fork_func(char *line)
 	 * pid esta guardando el resultado de fork, si es -1, dio error
 	 * si es 0 es el child
 	 */
-	char *args[] = {line, NULL};
-	pid_t pid = fork();
+	char *args[2];
+	pid_t pid;
+
+	args[0] = line;
+	args[1] = NULL;
+	pid = fork();
 
 	/**
 	 * if statement para validar si estamos en proceso hijo
