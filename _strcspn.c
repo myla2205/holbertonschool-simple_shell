b6e0f6c4ebb4f22c3508d55eb33d7affd7f80e30
cvalverde96 <cvalverde96@gmail.com>
2024-08-21 11:09:38 -0400

    subiendo _strcspn

---
+++ _strcspn.c
@@ -0,0 +1,19 @@
+#include "shell.h"
+
+size_t _strcspn(const char *s1, const char *s2)
+{
+        size_t ret = 0;
+
+        while (s1)
+        {
+                if(strchr(s2, *s1))
+                {
+                        return (ret);
+                }
+                else
+                {
+                        s1++,ret++;
+                }
+        }
+        return (ret);
+}
