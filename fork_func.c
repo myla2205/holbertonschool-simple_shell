f611d530d2ebe5e3938c869d35d83338e1d104a9
cvalverde96 <cvalverde96@gmail.com>
2024-08-21 09:57:09 -0400

    subiendo fork func con error nuevo

--- fork_func.c
+++ fork_func.c
@@ -9,32 +9,27 @@
 
 void fork_func(char *line, char *programa)
 {
-	/**
-	 * args es un array de strings, donde el primer elemento
-	 * line es el commando, y el segundo es NULL, para facilitar execve
-	 * pid esta guardando el resultado de fork, si es -1, dio error
-	 * si es 0 es el child
-	 */
+
 	char *args[2];
 	pid_t pid;
 
 	args[0] = line;
 	args[1] = NULL;
+
+	if (access(args[0], F_OK) == -1)
+	{
+		if (isatty(STDIN_FILENO))
+		{
+			perror(programa);
+		}
+	}
 	pid = fork();
 
-	/**
-	 * if statement para validar si estamos en proceso hijo
-	 * args[0] guarda el comando a ejecutarse
-	 * args es un array of strings que contiene los argumentos a ser
-	 * pasados al nuevo programa, segundo argumento es NULL para
-	 * que execve sepa que es el final de los argumentos
-	 * EXIT_FAILURe es un macro incluido en stdlib.h que retorna 1
-	 */
 	if (pid == 0)
 	{
 		if (execve(args[0], args, environ) == -1)
 		{
-			fprintf(stderr, "%s: 1: %s: not found\n", programa, args[0]);
+			perror(programa);
 			exit(EXIT_FAILURE);
 		}
 	}
