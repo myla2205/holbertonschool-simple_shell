c9a12a77da285d64a7a3d823d6f18ecf930099f2
Kamyl <myla2205@gmail.com>
2024-08-20 21:44:12 -0400

    update

--- shell.h
+++ shell.h
@@ -12,5 +12,5 @@ extern char **environ;
 void display_input(char **line, size_t *length);
 void fork_func(char *line, char *programa);
 int main(int ac, char **av);
-int main(__attribute__((unused)) int argc, char **argv);
+
 #endif
